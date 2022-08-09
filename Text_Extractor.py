22b008bbee975beb6fec8dc6fb3312de8d45facf
Justice Ohene Amofa <70078600+Joamofa1@users.noreply.github.com>
2022-08-09 23:33:44 +0000

    extractor.py

---
+++ Text_Extractor.py
@@ -0,0 +1,17 @@
+def pdf_extract(file, i):
+  print("extracting from file:", file)
+  delete_ppms()
+  images = pdf2image.convert_from_path(PATH + file, output_folder=PATH)
+  j = 0
+  for file in sorted (os.listdir(PATH)):
+      if '.ppm' in file and 'image' not in file:
+        os.rename(PATH + file, PATH + 'image' + str(i) + '-' + str(j) + '.ppm')
+        j += 1
+  j = 0
+  f = open(PATH +'result{}.txt'.format(i), 'w')
+  files = [f for f in os.listdir(PATH) if '.ppm' in f]
+
+  for file in sorted(files, key=lambda x: int(x[x.index('-') + 1: x.index('.')])):
+      temp = pytesseract.image_to_string(Image.open(PATH + file))
+      f.write(temp)
+  f.close()
